5efd80c94b7796ed9241e1bb1a8333773814ee52
Eustaquio Rangel <taq@eustaquiorangel.com>
2010-04-21 12:35:40 -0300

    Password 'drying'

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import glib
 import gconf
 
 keyring = False
@@ -13,29 +14,38 @@ class Config:
       self.password  = None
       self.gconf     = gconf.client_get_default()
 
-      self.interval = self.gconf.get_int("/apps/gtracker/interval")
+      self.username_key   = "/apps/gtracker/username"
+      self.password_key   = "/apps/gtracker/password"
+      self.interval_key   = "/apps/gtracker/interval"
+
+      self.interval = self.gconf.get_int(self.interval_key)
       if self.interval<1:
          self.interval = 15
-         self.gconf.set_int("/apps/gtracker/interval",self.interval)
+         self.gconf.set_int(self.interval_key,self.interval)
 
       if not keyring:
-         self.username = self.gconf.get_string("/apps/gtracker/username")
+         self.username = self.gconf.get_string(self.username_key)
          if self.username==None or len(self.username)<1:
             self.username = ""
-            self.gconf.set_string("/apps/gtracker/username","")
+            self.gconf.set_string(self.username_key,"")
 
-         self.password = self.gconf.get_string("/apps/gtracker/password")
+         self.password = self.gconf.get_string(self.password_key)
          if self.password==None or len(self.password)<1:
             self.password = ""
-            self.gconf.set_string("/apps/gtracker/password",self.password)
+            self.gconf.set_string(self.password_key,self.password)
+      else:
+         self.gconf.unset(self.username_key)
+         self.gconf.unset(self.password_key)
+         self.gconf.unset(self.interval_key)
+         glib.set_application_name("Gtracker")
 
    def save(self,username,password,interval):
       try:
-         self.gconf.set_int("/apps/gtracker/interval",interval)
+         self.gconf.set_int(self.interval_key,interval)
 
          if not keyring:
-            self.gconf.set_string("/apps/gtracker/username",username)
-            self.gconf.set_string("/apps/gtracker/password",password)
+            self.gconf.set_string(self.username_key,username)
+            self.gconf.set_string(self.password_key,password)
          else:
             pass
       except:
